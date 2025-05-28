142e8edadabe3d4ad08655a878a5ef2bea3871b8
Rémy <158462966+remyd06@users.noreply.github.com>
2025-05-28 18:57:50 +0200

    Syntaxe + tests

--- Module 02/ex00/source/main.cpp
+++ Module 02/ex00/source/main.cpp
@@ -15,11 +15,13 @@
 int main( void ) 
 {
 	Fixed a;
-	Fixed b( a  );
+	Fixed b(a);
 	Fixed c;
 	c = b;
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	c.setRawBits(15);
 	std::cout << c.getRawBits() << std::endl;
 	return 0;
 }
